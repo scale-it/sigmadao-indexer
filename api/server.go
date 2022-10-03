20e664604bbd6e6f3e5a9c460aab64511329622f
Ravinda <ravindra.iitism@gmail.com>
2022-10-03 22:55:22 +0530

    comment

--- api/server.go
+++ api/server.go
@@ -1,21 +1,6 @@
 package api
 
-import (
-	"context"
-	"net"
-	"net/http"
-	"time"
-
-	echo_contrib "github.com/labstack/echo-contrib/prometheus"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	log "github.com/sirupsen/logrus"
-
-	"github.com/algorand/indexer/api/generated/common"
-	"github.com/algorand/indexer/api/generated/v2"
-	"github.com/algorand/indexer/api/middlewares"
-	"github.com/algorand/indexer/idb"
-)
+import "time"
 
 // ExtraOptions are options which change the behavior or the HTTP server.
 type ExtraOptions struct {
@@ -69,95 +54,4 @@ type ExtraOptions struct {
 	// Applications
 	MaxApplicationsLimit     uint64
 	DefaultApplicationsLimit uint64
-}
-
-func (e ExtraOptions) handlerTimeout() time.Duration {
-	// Basically, if write timeout is 2 seconds or greater, subtract a second.
-	// If less, subtract 10% as a safety valve.
-	if e.WriteTimeout >= 2*time.Second {
-		return e.WriteTimeout - time.Second
-	}
-
-	return e.WriteTimeout - time.Duration(0.1*float64(e.WriteTimeout))
-}
-
-// Serve starts an http server for the indexer API. This call blocks.
-func Serve(ctx context.Context, serveAddr string, db idb.IndexerDb, fetcherError error, log *log.Logger, options ExtraOptions) {
-	e := echo.New()
-	e.HideBanner = true
-
-	if options.MetricsEndpoint {
-		p := echo_contrib.NewPrometheus("indexer", nil, nil)
-		if options.MetricsEndpointVerbose {
-			p.RequestCounterURLLabelMappingFunc = middlewares.PrometheusPathMapperVerbose
-		} else {
-			p.RequestCounterURLLabelMappingFunc = middlewares.PrometheusPathMapper404Sink
-		}
-		// This call installs the prometheus metrics collection middleware and
-		// the "/metrics" handler.
-		p.Use(e)
-	}
-
-	e.Use(middlewares.MakeLogger(log))
-	e.Use(middleware.CORS())
-
-	middleware := make([]echo.MiddlewareFunc, 0)
-
-	middleware = append(middleware, middlewares.MakeMigrationMiddleware(db))
-
-	if len(options.Tokens) > 0 {
-		middleware = append(middleware, middlewares.MakeAuth("X-Indexer-API-Token", options.Tokens))
-	}
-
-	swag, err := generated.GetSwagger()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	disabledMap, err := MakeDisabledMapFromOA3(swag, options.DisabledMapConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	api := ServerImplementation{
-		EnableAddressSearchRoundRewind: options.DeveloperMode,
-		db:                             db,
-		fetcher:                        fetcherError,
-		timeout:                        options.handlerTimeout(),
-		log:                            log,
-		disabledParams:                 disabledMap,
-		opts:                           options,
-	}
-
-	generated.RegisterHandlers(e, &api, middleware...)
-	common.RegisterHandlers(e, &api)
-
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	getctx := func(l net.Listener) context.Context {
-		return ctx
-	}
-	s := &http.Server{
-		Addr:           serveAddr,
-		ReadTimeout:    options.ReadTimeout,
-		WriteTimeout:   options.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
-		BaseContext:    getctx,
-	}
-
-	go func() {
-		if err := e.StartServer(s); err != nil {
-			log.Fatalf("Serve() err: %s", err)
-		}
-	}()
-
-	<-ctx.Done()
-	// Allow one second for graceful shutdown.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	if err := e.Shutdown(ctx); err != nil {
-		log.Fatal(err)
-	}
 }
